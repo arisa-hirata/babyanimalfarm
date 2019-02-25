655be4488c3d995d8fb56b5c2de02e4dd2428a7b
“arisa-hirata” <arisa.pirata@gmail.com>
2019-02-24 21:04:12 -0800

    delated

--- Landing/landing.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("about").addEventListener("click", function(){
-    document.getElementById("aboutPage").style.display = "block";
-    document.getElementById("welcome").style.display = "none";
-});
-
-document.getElementById("about2").addEventListener("click", function(){
-   document.getElementById("welcome").style.display = "block";
-    document.getElementById("aboutPage").style.display = "none";
-});
